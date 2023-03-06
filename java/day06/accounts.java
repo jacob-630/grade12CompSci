6c21295367fc98a2ad981cbe97e7293ff74c0cd0
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-03-05 20:49:42 -0500

    added appropriate documentation

--- java/day06/accounts.java
+++ java/day06/accounts.java
@@ -3,25 +3,43 @@ package day06;
 public class accounts {
     public static float savings;
     public static float chequings;
+    public static float credit;
 
     
-
+//Initalize the constructor for the accounts
     accounts(){}
 
+    //basic accounts model
     accounts(float savings, float chequings){
         this.savings = savings;
         this.chequings = chequings;
         System.out.println("savings: "+this.savings + " " + "chequings:" + this.chequings);
     }
+    //overload function for accounts, used for when credit is added
+    accounts(float savings, float chequings, float credit){
+        this.savings = savings;
+        this.chequings = chequings;
+        this.credit = credit;
+        System.out.println("savings: "+this.savings + " " + "chequings:" + this.chequings + "credit: "+this.credit);
+    }
+
+    //FUNCTIONS TO CHANGE ACCOUNTS
+
+
+
+    //deposit into savings
     public static  void savingsDepo(float amt){
         savings+=amt;
         System.out.println("savings: "+savings);
     }
+    //deposit into cequings
     public static void chequingsDepo(float amt){
         chequings+=amt;
-        System.out.println("savings: "+chequings);
+        System.out.println("chequings: "+chequings);
     }
+    //withdraw from savings
     public static void savingsWith(float amt){
+        //check that the requested amount is less than the account balance
         if(savings > amt)
             {savings-=amt;
             System.out.println("savings: "+savings);
@@ -30,7 +48,9 @@ public class accounts {
             System.out.println("Insifficent funds in the selected account.");
         }
     }
+    //withdraw from chequings
     public static void chequingWith(float amt){
+        //check that the requested amount is less than the account balance
         if(chequings > amt)
         {savings-=amt;
         System.out.println("chequings: "+chequings);
@@ -39,9 +59,11 @@ public class accounts {
         System.out.println("Insifficent funds in the selected account.");
     }
     }
-    public static void credit(){
+    //check how much credit the account holder is eligible for
+    public static float credit(){
         float total = (chequings + savings)/2;
         System.out.println("you are approved for credit up to $"+total);
+        return total;
     }
 
 }
--- java/day06/main.java
+++ java/day06/main.java
@@ -2,12 +2,15 @@ package day06;
 import java.util.Scanner;;
 public class main {
     public static void main(String[] args) {
+        //create the scanner for input
         Scanner inp = new Scanner(System.in);
         System.out.println("Would you like to enter your account?");
         String ans = (inp.next()).toLowerCase();
+        //check that they do wish to enter account
         if(ans.equals("yes") ){
             System.out.println("input your savings account balance followed "
             +"by your checking account balance");
+            //convert balances to floats and then create an instance of accounts
             String savings = inp.next();
             String chequings = inp.next();
             float savingsBal = Float.valueOf(savings);
@@ -15,14 +18,19 @@ public class main {
             accounts d = new accounts(savingsBal, chequingsBal);
             
             //Make changes to accounts:
+            //a loop to run for forever unless broken with 'break' command
             while(1 < 2){
                 System.out.println("What would you like to do? enter "
                 +"'deposit', 'withdraw', 'credit', or anything else to leave");
-                String chngs = inp.next();
+                String chngs = inp.next().toLowerCase();
+                //first layer of if statements; check which account the user would like to manipulate:
                 if(chngs.equals("deposit")){
                     System.out.println("Which account would you like to change? ('savings' or 'chequings')");
-                    String acc = inp.next();
+                    String acc = inp.next().toLowerCase();
 
+
+                    //navigate into those functions and call the specific commands to make the 
+                    //appropriate changes
                     if(acc.equals("savings")){
                         System.out.println("How much would you like to deposit?");
                         String depo = inp.next();
@@ -35,27 +43,33 @@ public class main {
                         Float depAmt = Float.valueOf(depo);
                         accounts.chequingsDepo(depAmt);
                     }
+                    //default case with a System exit to end the program
                     else{
                         System.out.println("Invalid input, goodbye");
                         System.exit(0);
                     }
                 }
+
+                //run all the code to withdraw funds from an account
                 else if(chngs.equals("withdraw")){
                     System.out.println("Which account would you like to change? ('savings' or 'chequings')");
                     String acc = inp.next();
 
+                    //withdraw from savings account
                     if(acc.equals("savings")){
                         System.out.println("How much would you like to withdraw?");
                         String depo = inp.next();
                         Float depAmt = Float.valueOf(depo);
                         accounts.savingsWith(depAmt);
                     }
+                    //withdraw from chequings
                     else if(acc.equals("chequings")){
-                        System.out.println("How much would you like to deposit?");
+                        System.out.println("How much would you like to withdraw?");
                         String depo = inp.next();
                         Float depAmt = Float.valueOf(depo);
                         accounts.chequingWith(depAmt);
                     }
+                    //else statement to catch all exceptions
                     else{
                         System.out.println("Invalid input, goodbye");
                         System.exit(0);
@@ -63,6 +77,7 @@ public class main {
                 }
                 else if(chngs.equals("credit")){
                     accounts.credit();
+                    d = new accounts(savingsBal, chequingsBal, accounts.credit());
                 }
                 else{
                     System.out.println("okay, have a good day");
@@ -71,4 +86,5 @@ public class main {
             }
         }
     }
+
 }
