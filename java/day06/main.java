02a6c7d2175ee498aad80764d506181b069e1714
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-03-05 23:43:41 -0500

    incorporated a compound interest calculation

--- java/day06/main.java
+++ java/day06/main.java
@@ -2,12 +2,15 @@ package day06;
 import java.util.Scanner;;
 public class main {
     public static void main(String[] args) {
+        //create a pin for the user
+        int pin = (int) Math.round(Math.random()*10000 - 1);
+        System.out.println("your pin will be " + pin);
         //create the scanner for input
         Scanner inp = new Scanner(System.in);
-        System.out.println("Would you like to enter your account?");
-        String ans = (inp.next()).toLowerCase();
+        System.out.println("Would you like to enter your account? if so please enter your pin.");
+        int ans = (inp.nextInt());
         //check that they do wish to enter account
-        if(ans.equals("yes") ){
+        if(ans == pin){
             System.out.println("input your savings account balance followed "
             +"by your checking account balance");
             //convert balances to floats and then create an instance of accounts
@@ -21,7 +24,7 @@ public class main {
             //a loop to run for forever unless broken with 'break' command
             while(1 < 2){
                 System.out.println("What would you like to do? enter "
-                +"'deposit', 'withdraw', 'credit', or anything else to leave");
+                +"'deposit', 'withdraw', 'credit', 'cmpndInt' or anything else to leave");
                 String chngs = inp.next().toLowerCase();
                 //first layer of if statements; check which account the user would like to manipulate:
                 if(chngs.equals("deposit")){
@@ -79,11 +82,30 @@ public class main {
                     accounts.credit();
                     d = new accounts(savingsBal, chequingsBal, accounts.credit());
                 }
+
+                //input a compound interest calculation that can also hand additional annual contributions
+                else if(chngs.equals("cmpndint")){
+                    System.out.println("input your principal investment, followed by your expected anual rate of return " + 
+                    "then your hold duration in years, and finally your additional contribution amount per year.");
+                    float P = Float.valueOf(inp.next());
+                    float R = Float.valueOf(inp.next())/100;
+                    float N = 1;
+                    float T = Float.valueOf(inp.next());
+                    float PMT = Float.valueOf(inp.next());
+                    float sum = (float) Math.round(((P * (Math.pow((R/N+1), (N*T)))) + PMT * ((Math.pow((R/N +1), (N*T))) - 1)/(R/N))*100)/100;
+                    
+                    System.out.println("The sum of the money you will make is " + sum);
+
+                }
                 else{
                     System.out.println("okay, have a good day");
                     System.exit(0);
                 }
             }
+        }
+        else{
+            System.out.println("Invalid Pin, access denied.");
+            System.exit(0);
         }
     }
 
