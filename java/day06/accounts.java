a089081294bf72bb0263b4a40b230e7572994555
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-03-11 10:55:05 -0500

    rephrased user prompts

--- java/day06/accounts.java
+++ java/day06/accounts.java
@@ -13,14 +13,14 @@ public class accounts {
     accounts(float savings, float chequings){
         this.savings = savings;
         this.chequings = chequings;
-        System.out.println("savings: "+this.savings + " " + "chequings: " + this.chequings);
+        System.out.println("savings: $"+this.savings + " " + "chequings: $" + this.chequings);
     }
     //overload function for accounts, used for when credit is added
     accounts(float savings, float chequings, float credit){
         this.savings = savings;
         this.chequings = chequings;
         this.credit = credit;
-        System.out.println("savings: "+this.savings + " " + "chequings:" + this.chequings + " credit: "+this.credit);
+        System.out.println("savings: $"+this.savings + " " + "chequings: $" + this.chequings + " credit: $"+this.credit);
     }
 
     //FUNCTIONS TO CHANGE ACCOUNTS ARE KEPT IN THE ACCOUNTCHANGES CLASS
