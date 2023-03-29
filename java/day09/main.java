3e8cc8427a3f9f5757fc6576340d447430c6459d
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-03-28 22:49:11 -0400

    initial commit and creating question and answer arrays

---
+++ java/day09/main.java
@@ -0,0 +1,22 @@
+package day09;
+import java.util.Scanner;
+public class main{
+    
+    public static void main(String[] args) {
+        String[] trueAnswers = {"456", "purple", "1910", "carbon", "brown", "four", "earth", "blue whale", "50", "brad"};
+        String[] questions = {"1: what is the sum of 100 and 356?", "2: what is the resultant colour when you mix red and blue?",
+        "3: what is 955 x 2?", "4: what is the 6th element on the periodic table?", "5: what is the most common eyecolour?", 
+        "6: fill in the blank there is one leap year every ____(spelt out) years", "7: what is the third closest planet to the sun",
+        "8: what is the largest animal to ever live?", "9: what is the answer to the question: 100-25*2", "10: if brads mom has 4 sons and three are named " + 
+        "North, South, and East, what is the last sons name?"}; 
+        String[] playerAnswers = new String[10];
+        Scanner inp = new Scanner(System.in);
+        System.out.println("answer each question as it is prompted");
+        for(int i = 0; i < 10; i++){
+        System.out.println(questions[i]);
+        String a = inp.nextLine().toLowerCase();
+        playerAnswers[i] = a;
+        }
+        System.out.println(playerAnswers[3]);
+    }
+}
