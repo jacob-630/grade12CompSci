0e73cc2c49e937f96be7bb96f88b147b56f01f8d
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-04-06 11:48:45 -0400

    created functions and added code to run them when called

--- java/day11/functions.java
+++ java/day11/functions.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
 public class functions {
-    
+    public static void printArray(ArrayList array){
+        for(int i = 0; i < array.size(); i++){
+            System.out.println(array.get(i));
+        }
+    }
+    public static void addElement(ArrayList array, String element){
+        array.add(element);
+    }
+    public static void changeElement(ArrayList array, int elementPos, String elementChng){
+        if(elementPos >= 0 && elementPos < array.size()){
+            array.set(elementPos, elementChng); 
+        }
+        else{
+            System.out.println("That element position does not exist in this array.");
+        }
+    }
+    public static void removeElement(ArrayList array, int elementPos){
+        if(elementPos >= 0 && elementPos < array.size()){
+            array.remove(elementPos); 
+        }
+        else{
+            System.out.println("That element position does not exist in this array.");
+        }
+    }
+
 }
--- java/day11/main.java
+++ java/day11/main.java
@@ -8,6 +8,9 @@ public class main {
         boolean cont = true;
         while(cont){
             //print intro and options of what to do
+            System.out.println(" ");
+            System.out.println(" ");
+            System.out.println(" ");
             System.out.println("What would you like to do with your array list?");
             System.out.println("Enter '1' to print array list");
             System.out.println("Enter '2' to add to your array list");
@@ -20,22 +23,37 @@ public class main {
 
             if(action.equals("1")){
                 //Check if the array list is empty
-                if(userArray.size() == 0){
+                if(userArray.isEmpty()){
                     System.out.println("Your array is currently empty");
                 }
                 //Print the size of the users array
+                else{
+                    functions.printArray(userArray);
+                }
 
             }
             else if(action.equals("2")){
                 //Add an element to the users array
+                System.out.println("Please enter the element you wish to add to the array");
+                String element = inp.nextLine();
+                functions.addElement(userArray, element);
 
             }
             else if(action.equals("3")){
                 //Take 2 paramaters, one for element number and one for what to change it to
+                System.out.println("Please enter the integer of the position youd like to change,"+
+                " followed by what youd like to change it to");
+                int pos = inp.nextInt();
+                String element = inp.nextLine();
+
+                functions.changeElement(userArray, pos, element);
 
             }
             else if(action.equals("4")){
                 //Remove an element from array (Take element number)
+                System.out.println("Enter the integer of the position you'd like to remove");
+                int ele = inp.nextInt();
+                functions.removeElement(userArray, ele);
 
             }
             else if(action.equals("5")){
