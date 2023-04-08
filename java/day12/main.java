47d4eaa2ad6e4dad8aabdb9a6c38d26dffbe223c
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-04-08 02:46:11 -0400

    continued planning

--- java/day12/main.java
+++ java/day12/main.java
@@ -1,8 +1,15 @@
 package day12;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-        
+        Scanner inp = new Scanner(System.in);
+        System.out.println(/*Game lore and intro sorta thing */);
+        //have random number generators to make certain events RNG based
+        //have choices with consequences ie.
+        //"allocate what percent to attack v defence"you focused to much on attack and died
+        //have a rpg kind of dialogue, potentially multiple choice and numeric input
     }
     
 }
