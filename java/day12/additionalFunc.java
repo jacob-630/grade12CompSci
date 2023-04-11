066f384d84ce7caeb5eeaa430347aa57cbbc019f
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-04-11 00:28:29 -0400

    created new file for additional functions

---
+++ java/day12/additionalFunc.java
@@ -0,0 +1,14 @@
+package day12;
+import java.util.concurrent.TimeUnit;
+
+public class additionalFunc {
+    public static void pause(int seconds){
+        try{
+            TimeUnit.SECONDS.sleep(seconds);
+        }
+        catch(InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    
+}
