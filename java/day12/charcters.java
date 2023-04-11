a12beec3583a287b1304cba7952ba6db22b9fcfa
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-04-10 23:56:45 -0400

    fixing attack function

--- java/day12/charcters.java
+++ java/day12/charcters.java
@@ -43,17 +43,24 @@ public class charcters {
 
     public static boolean battle(ArrayList player, ArrayList enemy){
 
-        while(Integer.valueOf((String) player.get(0)) > 0 && Integer.valueOf((String) enemy.get(0)) > 0){
+        int pHealth = Integer.valueOf((String) player.get(0));
+        int eHealth = Integer.valueOf((String) enemy.get(0));
+        int eSpeed = Integer.valueOf((String) player.get(2));
+        int pSpeed = Integer.valueOf((String) player.get(2));
+        int eDamage = Integer.valueOf((String) enemy.get(1));
+        int pDamage = Integer.valueOf((String) enemy.get(1));
+        while(pHealth > 0 && eHealth > 0){
             //check who has a greater speed value Enemy or Player
             //if the enemy is faster
-            if(Integer.valueOf((String) enemy.get(2)) > Integer.valueOf((String) player.get(2)) ){
+            if(eSpeed > pSpeed){
                 //Subtract the enemys damage from the players health 
-                int enemyDamage = Integer.valueOf((String) enemy.get(1));
-                player.set(0, (Integer.valueOf((String) player.get(0)) - enemyDamage));
+                player.set(0, (pHealth-eDamage));
+                pHealth -= eDamage;
 
                 //subtract the players damage from the enemys health
-                int playerDamage = Integer.valueOf((String) player.get(1));
-                enemy.set(0, (Integer.valueOf((String) enemy.get(0)) - playerDamage));
+                enemy.set(0, (eHealth-pDamage));
+                eHealth -= pDamage;
+
             }
             //else the player is faster and attackes first
             else{
