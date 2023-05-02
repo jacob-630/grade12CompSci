89ec9732be959ad4c7da5f1af624f2351d117fb3
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-05-02 16:58:24 -0400

    added a new menu style

--- python/day03.py
+++ python/day03.py
@@ -1,34 +1,27 @@
 import random
 
-array = []
-for i in range(5):
-    array.append(random.randint(1, 6))
-print(array)
-while True:
-    end = input("1. to reroll, 2. to lock in a number, 3 to finish and tally score? ")
-    if end == 1:
-        for i in array:
-            i = random.randint(1, 6)
-    elif end == 2:
-        og = int(input("what index would you like to lock in (0 indexed)? "))
-        new = int(input("what would you like to change these numbers to? "))
-        for i in range(len(array)):
-            if array[i] == og:
-                array[i] = new
-                totalChng += 1
-    else:
-        elementChng = int(input("Which element would you like to change? (note: arrays are zero indexed) "))
-        if 0 > elementChng or elementChng > length:
-            print("This index does not exist in this array.")
-            continue;
-        newEl = int(input("what number (between 1 and 6 inclusive) do you wish to change it to? "))
-        if 1 > newEl or newEl > 6:
-            print("The number entered is not between 1 and 6.")
-            continue
-        array[elementChng] = newEl; 
-        totalChng += 1;
-    print(array);
-print("Your score is "+ str(sum(array)) +" out of " + str(length * 6) + " thats " + str(int((sum(array) / (length * 6)) * 100)) + "%, "+
-      "you also changed a total of " + str(totalChng) + " numbers.")   
 
+con = True
+
+dice = []
+final = []
+for i in range(5):
+    dice.append(random.randint(1, 6))
+
+print(dice)
+while(con):
+    choice = input("What would you like to do, 1. reroll, 2. lock in a number, 3. finish and tally points: ")
+    if choice == "1":
+        for i in range(5):
+            if i not in final:
+                dice[i] = random.randint(1, 5)
+                
+        print(dice);
+    elif choice == "2":
+        lockIn = input("What index would you like to lock in? (0 indexed)")
+        final.append(lockIn)
+
+    elif choice == "3":
+        print("three")
+        con = False
 
