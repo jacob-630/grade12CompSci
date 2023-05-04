1d7c51e1ff1ceb518c4dedf6d47e594d69a540c4
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-05-03 20:03:07 -0400

    added new feature: lock in numbers:

--- python/day03.py
+++ python/day03.py
@@ -12,13 +12,14 @@ print(dice)
 while(con):
     choice = input("What would you like to do, 1. reroll, 2. lock in a number, 3. finish and tally points: ")
     if choice == "1":
-        for i in range(5):
-            if i not in final:
-                dice[i] = random.randint(1, 5)
-                
-        print(dice);
+       print(dice)
+       for i in range(5):
+           if i not in final:
+               dice[i] = random.randint(1, 6)
+           
+    
     elif choice == "2":
-        lockIn = input("What index would you like to lock in? (0 indexed)")
+        lockIn = int(input("What index would you like to lock in? (0 indexed: )"))
         final.append(lockIn)
 
     elif choice == "3":
