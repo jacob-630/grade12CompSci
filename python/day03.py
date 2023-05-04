027525358a06485ae5e322ff8cf34c777d6df22c
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-05-04 10:28:07 -0400

    completed assignment

--- python/day03.py
+++ python/day03.py
@@ -8,6 +8,29 @@ def addDice(dice):
     
     return diceSum
 
+def tallyPoints(dice):
+    score = 0
+    dice.sort()
+    #Check for Yahtzee
+    if(dice[0] == dice[4]):
+        score += 50
+    #Check for large straight
+    elif ((dice[0] + dice[4] == 6) and (dice[1] + dice [3] == 6) and (dice[2] * 2 == 6)):
+        score += 40
+    elif((dice[0] + dice[4] == 8) and (dice[1] + dice[3] == 8) and (dice[2] * 2 == 6)):
+        score += 40
+    #Check for Small straight
+    elif((dice[0] + dice [3] == 5) and (dice[1] + dice[2] == 5)):
+        score += 30
+    elif((dice[1] + dice [4] == 9) and (dice[2] + dice[3] == 9)):
+        score += 30 
+    #Check for four of a kind
+    elif(dice[0] == dice[3] or dice[1] == dice[4]):
+        score += 25
+    #Check for three of a kind
+    elif((dice[2] == dice[4]) or (dice[1] == dice[3]) or (dice[0] == dice[2])):
+        score += 20
+    return score
 
 con = True
 
@@ -33,7 +56,8 @@ while(con):
         final.append(lockIn)
 
     elif choice == "3":
-        print(addDice(dice))
+        
+        print(tallyPoints(dice) + addDice(dice))
         con = False
 
 
@@ -41,25 +65,3 @@ while(con):
 
 
 
-def tallyPoints(dice):
-    score = 0
-    dice = dice.sort()
-    #Check for Yahtzee
-    if(dice[0] == dice[4]):
-        score += 50
-    #Check for large straight
-    elif ((dice[0] + dice[4] == 6) and (dice[1] + dice [3] == 6) and (dice[2] * 2 == 6)):
-        score += 40
-    elif((dice[0] + dice[4] == 8) and (dice[1] + dice[3] == 8) and (dice[2] * 2 == 6)):
-        score += 40
-    #Check for Small straight
-    elif((dice[0] + dice [3] == 5) and (dice[1] + dice[2] == 5)):
-        score += 30
-    elif((dice[1] + dice [4] == 9) and (dice[2] + dice[3] == 9)):
-        score += 30 
-    #Check for four of a kind
-    elif(dice[0] == dice[3] or dice[1] == dice[4]):
-        score += 25
-    #Check for three of a kind
-    elif((dice[2] == dice[4]) or (dice[1] == dice[3]) or (dice[0] == dice[2])):
-        score += 20
