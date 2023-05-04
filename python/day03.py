7b37e941ddd9b28252b95d33bcc4614054508323
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-05-04 10:06:32 -0400

    added scoring function

--- python/day03.py
+++ python/day03.py
@@ -1,6 +1,14 @@
 import random
 
 
+def addDice(dice):
+    diceSum = 0
+    for i in dice:
+        diceSum += i
+    
+    return diceSum
+
+
 con = True
 
 dice = []
@@ -12,10 +20,12 @@ print(dice)
 while(con):
     choice = input("What would you like to do, 1. reroll, 2. lock in a number, 3. finish and tally points: ")
     if choice == "1":
-       print(dice)
+       
        for i in range(5):
            if i not in final:
-               dice[i] = random.randint(1, 6)
+               dice[i] = random.randint(1, 6);
+       print(dice)
+        
            
     
     elif choice == "2":
@@ -23,6 +33,33 @@ while(con):
         final.append(lockIn)
 
     elif choice == "3":
-        print("three")
+        print(addDice(dice))
         con = False
 
+
+
+
+
+
+def tallyPoints(dice):
+    score = 0
+    dice = dice.sort()
+    #Check for Yahtzee
+    if(dice[0] == dice[4]):
+        score += 50
+    #Check for large straight
+    elif ((dice[0] + dice[4] == 6) and (dice[1] + dice [3] == 6) and (dice[2] * 2 == 6)):
+        score += 40
+    elif((dice[0] + dice[4] == 8) and (dice[1] + dice[3] == 8) and (dice[2] * 2 == 6)):
+        score += 40
+    #Check for Small straight
+    elif((dice[0] + dice [3] == 5) and (dice[1] + dice[2] == 5)):
+        score += 30
+    elif((dice[1] + dice [4] == 9) and (dice[2] + dice[3] == 9)):
+        score += 30 
+    #Check for four of a kind
+    elif(dice[0] == dice[3] or dice[1] == dice[4]):
+        score += 25
+    #Check for three of a kind
+    elif((dice[2] == dice[4]) or (dice[1] == dice[3]) or (dice[0] == dice[2])):
+        score += 20
