a90b8cab9fb6e379749709f425a8f2cbeda9f819
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-05-05 10:47:53 -0400

    fixed the small straight code

--- python/day03.py
+++ python/day03.py
@@ -8,27 +8,50 @@ def addDice(dice):
     
     return diceSum
 
+
+            
+
+            
 def tallyPoints(dice):
     score = 0
     dice.sort()
+
+
     #Check for Yahtzee
     if(dice[0] == dice[4]):
+        print("You have a Yahtzee")
         score += 50
+
+
     #Check for large straight
     elif ((dice[0] + dice[4] == 6) and (dice[1] + dice [3] == 6) and (dice[2] * 2 == 6)):
+        print("You have a large straight")
         score += 40
     elif((dice[0] + dice[4] == 8) and (dice[1] + dice[3] == 8) and (dice[2] * 2 == 6)):
+        print("You have a large straight")
         score += 40
+
+
     #Check for Small straight
-    elif((dice[0] + dice [3] == 5) and (dice[1] + dice[2] == 5)):
-        score += 30
-    elif((dice[1] + dice [4] == 9) and (dice[2] + dice[3] == 9)):
-        score += 30 
+    elif(dice[3] - dice[2] != 2) and (dice[2] - dice[1] != 2):
+        sStraight = 0
+        for i in range(4):
+            if(dice[i+1]-dice[i] == 1):
+                sStraight += 1
+        if sStraight == 3:
+            print("You have a small straight")
+            score += 30;
+    
+
     #Check for four of a kind
     elif(dice[0] == dice[3] or dice[1] == dice[4]):
+        print("You have four of a kind")
         score += 25
+
+
     #Check for three of a kind
     elif((dice[2] == dice[4]) or (dice[1] == dice[3]) or (dice[0] == dice[2])):
+        print("You have three of a kind")
         score += 20
     return score
 
@@ -56,8 +79,7 @@ while(con):
         final.append(lockIn)
 
     elif choice == "3":
-        
-        print(tallyPoints(dice) + addDice(dice))
+        print("your score is: " + str(tallyPoints(dice) + addDice(dice)))
         con = False
 
 
