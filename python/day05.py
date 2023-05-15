10865abdfbbc695ba43d4bba372b068c4cafbf09
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-05-15 08:46:08 -0400

    added calculations of dates

--- python/day05.py
+++ python/day05.py
@@ -1,35 +1,28 @@
 import datetime
 
+
 #get todays date and put it into a list
-dateToday = str(datetime.date.today())
-dateToday = dateToday.split("-")
-for i in range(len(dateToday)):
-    dateToday[i] = int(dateToday[i])
+dateToday = datetime.date.today()
 
-dateAge = round(dateToday[0] * 365.25)
-dateAge += (365.25/12 * dateToday[1])
-dateAge += dateToday[2]
+
 #collet the users information in the same form of todays date
-userDate = []
-userDate.append(int(input("What year were you born? ")))
-userDate.append(int(input("What month were you born (use number ex. January = 1)? ")))
-userDate.append(int(input("What day were you born? ")))
 
-#turn the users birthday into days
-userAge = round(userDate[0] * 365.25)
-userAge += (365.25/12 * userDate[1])
-userAge += userDate[2]
+userYear = (int(input("What year were you born? ")))
+userMonth = (int(input("What month were you born (use number ex. January = 1)? ")))
+userDay = (int(input("What day were you born? ")))
+
+userDate = datetime.date(userYear, userMonth, userDay)
 
 #find the users 21st birthday
-user21st = userAge + 7670
+turn21 = datetime.date(userYear + 21, userMonth, userDay)
 
 #Find how many days to or from the users 21st birthday
-tofrom21stArray = []
-toFrom21st = user21st - dateAge
-tofrom21stArray.append(toFrom21st % 365)
-toFrom21st = toFrom21st % 365
-tofrom21stArray.append(toFrom21st % (365.25/12))
-toFrom21st = round(toFrom21st % (365.25/12))
-tofrom21stArray.append(toFrom21st)
-print(tofrom21stArray)
+
+fromLastBday = dateToday - datetime.date(2022, userMonth, userDay)
+toNextBday = datetime.date(2023, userMonth, userDay) - dateToday
+tofrom21st = turn21 - dateToday
+
+print(fromLastBday)
+print(toNextBday)
+print(tofrom21st)
 
